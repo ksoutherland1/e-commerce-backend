5b60d2af3a2a9e20438ebe164fe7eb0753e8298d
jromatowski <joshuaromatowski@gmail.com>
2022-07-29 14:27:04 -0700

    cartitem schema change

--- models/CartItem.js
+++ models/CartItem.js
@@ -3,10 +3,9 @@ const mongoose = require('../db/connection');
 
 const CartItemSchema = new mongoose.Schema({
 
-	// Clean this up, make price and quantity number values
 	item: String,
-	price: String,
-	quantity: String,
+	price: Number,
+	quantity: Number,
 });
 
 const CartItem = mongoose.model('CartItem', CartItemSchema);
